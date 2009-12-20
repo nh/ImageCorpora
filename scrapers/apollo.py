5cbb9ffd3fd2273e84ea70e1dbb4310a236bfd05
Nathan Hackett <nhackett1@gmail.com>
2009-12-20 22:08:38 +0000

    More scraper updates

--- scrapers/apollo.py
+++ scrapers/apollo.py
@@ -3,6 +3,7 @@ import urlparse
 from urllib2 import urlopen
 from urllib import urlretrieve
 from funcs import baseconv
+from PIL import Image as im
 import os
 import sys
 import re
@@ -24,9 +25,13 @@ for row in rows:
         desc = text[2]
     else:
         desc = ""        
-    db.insert('imgs',title=title,desc=desc,added=web.SQLLiteral("datetime('now')"))
+    db.insert('imgs',title=title,desc=desc,filetype="jpg",added=web.SQLLiteral("datetime('now')"))
     img_id = str(db.query("SELECT MAX(id) as id FROM imgs")[0].id)
-    img_id = baseconv(img_id,False)
-    print title + " -- " + img_id
+    base_id = baseconv(img_id,False)
     url = "http://www.hq.nasa.gov/office/pao/History/alsj/a11/" + title + ".jpg"
-    #urlretrieve(url,"./"+img_id+".jpg")
+    dest_path = "./i/"+base_id+"/"
+    os.makedirs(dest_path)
+    dest = dest_path+img_id+".jpg"
+    urlretrieve(url,dest)
+    w, h = im.open(dest).size
+    db.update('imgs',where="id = " + img_id,base_id=base_id, w=w, h=h,filesize = os.path.getsize(dest))
